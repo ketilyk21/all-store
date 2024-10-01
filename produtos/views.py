a967193bfff660c1ad3989748bd335ef541165fd
Grazieleslv <graziele.silva@escolar.ifrn.edu.br>
2024-10-01 18:46:32 -0300

    criando a estrutura de templates do projeto

--- produtos/views.py
+++ produtos/views.py
@@ -1,23 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
-    ...
+    return render(request, "index.html")
+
 
 def product_create(request):
-    ...
+    return render(request, "produto_form.html")
+
 
 def product_update(request):
-    ...
+    return render(request, "produto_form.html")
+
 
 def product_delete(request):
-    ...
+    return redirect("index")
+
 
 def user_register(request):
-    ...
+    return render(request, "user_form.html")
+
 
 def user_login(request):
-    ...
+    return render(request, "user_form.html")
 
-def user_logout(request):
-    ...
+
+def user_logout(request): ...
